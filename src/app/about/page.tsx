6e9614bf8fb076e3958a7f65b693108d0821796c
Dave Donnelly <davepauldonnelly@gmail.com>
2025-07-22 23:05:07 +1000

    Added About page responsiveness

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,48 +14,58 @@ export default function About() {
       <Header />
       <main className={mainstyles.main}>
         <section className={styles.about}>
-          <img
-            src="./headshot_BW.jpg"
-            className={styles.headshot}
-            alt="A black and white photo of Tyson Donnelly smiling to camera"
-          />
-          <div className={styles.aboutPara}>
-            <h1>About</h1>
-            <p>
-              I&apos;m a VFX and Compositing Supervisor based in Sydney.
-              I&apos;m experienced in both client and vendor side VFX show
-              delivery.
-            </p>
+          <div className={styles.container}>
+            <img
+              src="./headshot_BW.jpg"
+              className={styles.headshot}
+              alt="A black and white photo of Tyson Donnelly smiling to camera"
+            />
+            <div className={styles.aboutPara}>
+              <h1>About</h1>
+              <p>
+                I&apos;m a VFX and Compositing Supervisor based in Sydney.
+                I&apos;m experienced in both client and vendor side VFX show
+                delivery.
+              </p>
+            </div>
           </div>
         </section>
         <section className={styles.contact}>
-          <h2>Contact</h2>
-          <ul>
-            <li>
-              <FontAwesomeIcon icon={faImdb} className={styles.contactIcons} />
-              <Link href="http://www.imdb.com/name/nm4286541/" target="_blank">
-                imdb.com
-              </Link>
-            </li>
-            <li>
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                className={styles.contactIcons}
-              />
-              <Link
-                href="https://www.linkedin.com/in/tyson-donnelly-66aa3a35"
-                target="_blank"
-              >
-                linkedin.com
-              </Link>
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faAt} className={styles.contactIcons} />
-              <Link href="mailto:tysondonnelly@gmail.com" target="_blank">
-                tysondonnelly@gmail.com
-              </Link>
-            </li>
-          </ul>
+          <div className={styles.container}>
+            <h2>Contact</h2>
+            <ul>
+              <li>
+                <FontAwesomeIcon
+                  icon={faImdb}
+                  className={styles.contactIcons}
+                />
+                <Link
+                  href="http://www.imdb.com/name/nm4286541/"
+                  target="_blank"
+                >
+                  imdb.com
+                </Link>
+              </li>
+              <li>
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  className={styles.contactIcons}
+                />
+                <Link
+                  href="https://www.linkedin.com/in/tyson-donnelly-66aa3a35"
+                  target="_blank"
+                >
+                  linkedin.com
+                </Link>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faAt} className={styles.contactIcons} />
+                <Link href="mailto:tysondonnelly@gmail.com" target="_blank">
+                  tysondonnelly@gmail.com
+                </Link>
+              </li>
+            </ul>
+          </div>
         </section>
       </main>
       <Footer />
